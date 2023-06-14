659a20454d32d373f3f6c98475302e6085cfd8de
Mahbub-diu <mahbub.mediasoft@gmail.com>
2023-06-14 23:27:09 +0600

    update final

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,7 @@
 const itemsBar = document.querySelectorAll('.hover-shape');
 const itemsBarArr = Array.from(itemsBar);
 
-const headbar = document.querySelector('.header_content_pos');
+const headbar = document.querySelector('.header-content-single');
 itemsBarArr.forEach((item) => {
   item.addEventListener('mouseover', (e) => {
     itemsBarArr.forEach((item) => {
@@ -13,7 +13,7 @@ itemsBarArr.forEach((item) => {
         item.getBoundingClientRect().left +
         item.getBoundingClientRect().width / 2 -
         document
-          .querySelector('.row.header_content_pos')
+          .querySelector('.row.header-content-single')
           .getBoundingClientRect().left,
       ease: 'back.out(2)',
     });
@@ -28,5 +28,28 @@ headbar.addEventListener('mouseleave', function () {
   gsap.to('.nav-item-shape', 0.5, {
     left: '50%',
     ease: 'back.out(2)',
+  });
+});
+
+// slider js
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
+    },
   });
 });
